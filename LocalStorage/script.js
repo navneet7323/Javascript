744625140707481e62749601a95ac9203536ba16
Navneet Kumar <navneetsingh70mth@gmail.com>
2026-07-11 17:02:17 +0530

    js

--- LocalStorage/script.js
+++ LocalStorage/script.js
@@ -1,30 +1,30 @@
-localStorage.setItem("name", "Navneet Kumar");
+// localStorage.setItem("name", "Navneet Kumar");
 
-document.getElementById("demo").innerHTML = localStorage.getItem("name");
+// document.getElementById("demo").innerHTML = localStorage.getItem("name");
 
-localStorage.removeItem("name");
+// localStorage.removeItem("name");
 
-function clickcount() {
-  if (localStorage.clickcount) {
-    localStorage.clickcount = Number(localStorage.clickcount) + 1;
-  } else {
-    localStorage.clickcount = 1;
-  }
-  document.getElementById("demo").innerHTML = localStorage.clickcount;
-}
-localStorage.clear();
+// function clickcount() {
+//   if (localStorage.clickcount) {
+//     localStorage.clickcount = Number(localStorage.clickcount) + 1;
+//   } else {
+//     localStorage.clickcount = 1;
+//   }
+//   document.getElementById("demo").innerHTML = localStorage.clickcount;
+// }
+// localStorage.clear();
 
-let user = {
-  Username: "nk123",
-  password: "qwerty",
-};
+// let user = {
+//   Username: "nk123",
+//   password: "qwerty",
+// };
 
-localStorage.setItem("user", JSON.stringify(user));
+// localStorage.setItem("user", JSON.stringify(user));
 
-let getData = localStorage.getItem("user");
+// let getData = localStorage.getItem("user");
 
-let data = JSON.parse(getData);
+// let data = JSON.parse(getData);
 
-data.Username = "wertyuio";
-localStorage.removeItem("Username");
-console.log(data);
+// data.Username = "wertyuio";
+// localStorage.removeItem("Username");
+// console.log(data);
